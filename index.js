3ff7e7d7ed6bd2ded5b5ae4d71f9d07e7b422e09
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 16:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let nId=0;
+class Neighborhood{
+  
+  constructor(name){
+    this.id=++nId;
+    this.name=name;
+  }
+  
+  
+}
